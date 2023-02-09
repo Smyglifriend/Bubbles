cc4a07251640a126e1cdc56104e42dad8785f2fc
smyglifriend <heits_rs001@mail.ru>
2023-02-09 22:09:53 +0300

    Fix folders

--- Bubble/Assets/Scripts/Events/ScriptableObjects/DescriptionBaseSO.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-/// <summary>
-/// Base class for ScriptableObjects that need a public description field.
-/// </summary>
-public class DescriptionBaseSO : SerializableBaseSO
-{
-    [TextArea] public string description;
-}
