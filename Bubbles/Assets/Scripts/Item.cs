3d843acb22922520e66ea98c7ee1e896f9becd84
smyglifriend <heits_rs001@mail.ru>
2023-02-11 12:03:36 +0300

    Add levels

--- Bubbles/Assets/Scripts/Item.cs
+++ Bubbles/Assets/Scripts/Item.cs
@@ -1,12 +1,28 @@
-using System;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Item : MonoBehaviour
 {
+    [SerializeField] private VoidEventChannelSO voidEventChannelSo;
+    private void Start()
+    {
+        GetUnusedItem()
+            .gameObject
+            .SetActive(false);
+    }
+
     protected virtual void OnMouseDown()
     {
-        var unused = transform.GetChild(0);
-        unused.gameObject.SetActive(true);
+        GetUnusedItem()
+            .SetActive(
+            true);
+        voidEventChannelSo.RaiseEvent();
+    }
+
+    private GameObject GetUnusedItem()
+    {
+        var modelTransform = transform.GetChild(0);
+        var unused = modelTransform.GetChild(0);
+
+        return unused.gameObject;
     }
 }
