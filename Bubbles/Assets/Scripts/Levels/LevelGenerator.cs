d4368fbda310094f72ad248032002c75dee9ee1f
smyglifriend <heits_rs001@mail.ru>
2023-04-13 15:30:38 +0300

    last fix

--- Bubbles/Assets/Scripts/Levels/LevelGenerator.cs
+++ Bubbles/Assets/Scripts/Levels/LevelGenerator.cs
@@ -1,32 +1,34 @@
-using NaughtyAttributes.Test;
 using UnityEngine;
-using UnityEngine.Rendering;
-using UnityEngine.Rendering.Universal.Internal;
 
 public class LevelGenerator : MonoBehaviour
 {
     [SerializeField] private LevelDataEventChanelSO chooseButtonlevelDataEventChanelSo;
     [SerializeField] private LevelData defaultLevel;
-
     [SerializeField] private Camera camera;
-    [SerializeField] private float specialValue = 1;
+    [SerializeField] private float specialValue;
     [SerializeField] private float baseSpecialValue = 1;
-
-    private void Update()
-    {
-        camera.fieldOfView = baseSpecialValue + (float)Screen.height / Screen.width * specialValue;
-        Debug.Log((float)Screen.height / Screen.width);
-    }
 
     private LevelData _level;
     private GameObject _levelPrefab;
     private Vector3 _spawnBubblePosition = new(0f, -0.21f, 7.6f);
     private Vector3 _spawnPopItPosition = new(0f, 0.18f, 8.5f);
     private Vector3 _spawnSoapBallsPosition = new(0f, 0f, 14f);
+    private int _currentScreenWidth;
+    private int _currentScreenHeight;
 
+
+    private void Update()
+    {
+        Debug.Log((float)Screen.height / Screen.width);
+        if (_currentScreenWidth == Screen.width && _currentScreenHeight == Screen.height) return;
+        SetCameraPostion(_level);
+    }
 
     private void Start()
     {
+        _currentScreenWidth = Screen.width;
+        _currentScreenHeight = Screen.height;
+
         chooseButtonlevelDataEventChanelSo.RaiseEvent(defaultLevel);
         SetLevel(defaultLevel);
     }
@@ -77,106 +79,110 @@ public class LevelGenerator : MonoBehaviour
 
     private void InstantiateBubbleLevel()
     {
-        if ((float)Screen.height / Screen.width <= 1.2f && (float)Screen.height / Screen.width >= 1f)
-        {
-            specialValue = 43.34f;
-        }
-        else if ((float)Screen.height / Screen.width <= 1f && (float)Screen.height / Screen.width >= 0.8f)
-        {
-            specialValue = 56.6f;
-        }
-        else if ((float)Screen.height / Screen.width >= 0.5f && (float)Screen.height / Screen.width <= 0.6f)
-        {
-            specialValue = 79.22f;
-        }
-        else if ((float)Screen.height / Screen.width >= 0.6f && (float)Screen.height / Screen.width <= 0.689f)
-        {
-            specialValue = 67.8f;
-        }
-        else if ((float)Screen.height / Screen.width >= 0.7f && (float)Screen.height / Screen.width <= 0.8f)
-        {
-            specialValue = 60.7f;
-        }
-        else if ((float)Screen.height / Screen.width >= 1.2f)
-        {
-            specialValue = 38.74f;
-        }
-        else if((float)Screen.height / Screen.width >= 1f && (float)Screen.height / Screen.width <= 1.1f )
-        {
-            specialValue = 34.81f;
-        }
-        else
-        {
-            specialValue = 85.8f;
-        }
-
-        camera.fieldOfView = baseSpecialValue + (float)Screen.height / Screen.width * specialValue;
-
         _levelPrefab = Instantiate(_level.LvlPrefab, _spawnBubblePosition, Quaternion.Euler(-180f, 0, 0), transform);
     }
 
     private void InstantiatePopItLevel()
     {
-        if ((float)Screen.height / Screen.width <= 1.2f && (float)Screen.height / Screen.width >= 1f)
-        {
-            specialValue = 34.06f;
-        }
-        else if ((float)Screen.height / Screen.width <= 1f && (float)Screen.height / Screen.width >= 0.79f)
-        {
-            specialValue = 36.99f;
-        }
-        else if ((float)Screen.height / Screen.width >= 0.5f && (float)Screen.height / Screen.width <= 0.6f)
-        {
-            specialValue = 51.78f;
-        }
-        else if ((float)Screen.height / Screen.width >= 0.6f && (float)Screen.height / Screen.width <= 0.689f)
-        {
-            specialValue = 43.32f;
-        }
-        else if ((float)Screen.height / Screen.width >= 0.7f && (float)Screen.height / Screen.width <= 0.79f)
-        {
-            specialValue = 43.55f;
-        }
-        else if ((float)Screen.height / Screen.width >= 1.2f)
-        {
-            specialValue = 31.34f;
-        }
-        else if ((float)Screen.height / Screen.width >= 1f && (float)Screen.height / Screen.width <= 1.1f)
-        {
-            specialValue = 34.81f;
-        }
-        else
-        {
-            specialValue = 62.69f;
-        }
-
-        camera.fieldOfView = baseSpecialValue + (float)Screen.height / Screen.width * specialValue;
-
         _levelPrefab = Instantiate(_level.LvlPrefab, _spawnPopItPosition, Quaternion.Euler(0, 0, 0), transform);
     }
 
     private void InstantiateSoapBallsLevel()
     {
-        if ((float)Screen.height / Screen.width <= 1.2f && (float)Screen.height / Screen.width >= 0.8f)
-        {
-            specialValue = 57f;
-        }
-        else if((float)Screen.height / Screen.width > 1)
-        {
-            specialValue = 29.3f;
-        }
-        else
-        {
-            specialValue = 65.9f;
-        }
-        camera.fieldOfView = baseSpecialValue + (float)Screen.height / Screen.width * specialValue;
-     
         _levelPrefab = Instantiate(_level.LvlPrefab, _spawnSoapBallsPosition, Quaternion.identity, transform);
     }
 
-    private void SetCameraPostion(Vector3 position)
+    private void SetCameraPostion(LevelData _levelData)
     {
-        Camera.main.transform.position = position;
-        Debug.Log(Camera.main.transform.position);
+        if (_levelData.LevelName == "Bubble")
+        {
+            if ((float)Screen.height / Screen.width <= 1.2f && (float)Screen.height / Screen.width >= 1f)
+            {
+                specialValue = 43.34f;
+            }
+            else if ((float)Screen.height / Screen.width <= 1f && (float)Screen.height / Screen.width >= 0.8f)
+            {
+                specialValue = 56.6f;
+            }
+            else if ((float)Screen.height / Screen.width >= 0.5f && (float)Screen.height / Screen.width <= 0.6f)
+            {
+                specialValue = 79.22f;
+            }
+            else if ((float)Screen.height / Screen.width >= 0.6f && (float)Screen.height / Screen.width <= 0.689f)
+            {
+                specialValue = 67.8f;
+            }
+            else if ((float)Screen.height / Screen.width >= 0.7f && (float)Screen.height / Screen.width <= 0.8f)
+            {
+                specialValue = 60.7f;
+            }
+            else if ((float)Screen.height / Screen.width >= 1.2f)
+            {
+                specialValue = 38.74f;
+            }
+            else if ((float)Screen.height / Screen.width >= 1f && (float)Screen.height / Screen.width <= 1.1f)
+            {
+                specialValue = 34.81f;
+            }
+            else
+            {
+                specialValue = 85.8f;
+            }
+        }
+        else if (_levelData.LevelName == "Pop It")
+        {
+            if ((float)Screen.height / Screen.width <= 1.2f && (float)Screen.height / Screen.width >= 1f)
+            {
+                specialValue = 34.06f;
+            }
+            else if ((float)Screen.height / Screen.width <= 1f && (float)Screen.height / Screen.width >= 0.79f)
+            {
+                specialValue = 36.99f;
+            }
+            else if ((float)Screen.height / Screen.width >= 0.5f && (float)Screen.height / Screen.width <= 0.6f)
+            {
+                specialValue = 51.78f;
+            }
+            else if ((float)Screen.height / Screen.width >= 0.6f && (float)Screen.height / Screen.width <= 0.689f)
+            {
+                specialValue = 43.32f;
+            }
+            else if ((float)Screen.height / Screen.width >= 0.7f && (float)Screen.height / Screen.width <= 0.79f)
+            {
+                specialValue = 43.55f;
+            }
+            else if ((float)Screen.height / Screen.width >= 1.2f)
+            {
+                specialValue = 31.34f;
+            }
+            else if ((float)Screen.height / Screen.width >= 1f && (float)Screen.height / Screen.width <= 1.1f)
+            {
+                specialValue = 34.81f;
+            }
+            else if ((float)Screen.height / Screen.width == 1f)
+            {
+
+            }
+            else
+            {
+                specialValue = 62.69f;
+            }
+        }
+        else if (_levelData.LevelName == "Soap balls")
+        {
+            if ((float)Screen.height / Screen.width <= 1.2f && (float)Screen.height / Screen.width >= 0.8f)
+            {
+                specialValue = 57f;
+            }
+            else if ((float)Screen.height / Screen.width > 1)
+            {
+                specialValue = 29.3f;
+            }
+            else
+            {
+                specialValue = 65.9f;
+            }
+        }
+        camera.fieldOfView = baseSpecialValue + (float)Screen.height / Screen.width * specialValue;
     }
 }
--- Bubbles/Assets/Scripts/UIMenu/Button Animations/SoundButtonAnimation.cs
+++ Bubbles/Assets/Scripts/UIMenu/Button Animations/SoundButtonAnimation.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class SoundButtonAnimation : MonoBehaviour
 {
     [SerializeField] private Button button;
-    [SerializeField] private AudioSource audioSource;
+    
     private Animator animator;
 
     private int _clickCount;
@@ -22,12 +22,10 @@ public class SoundButtonAnimation : MonoBehaviour
         animator.SetTrigger("IsClicked");
         if (_clickCount == 0)
         {
-            audioSource.Stop();
             _clickCount= 1;
         }
         else
         {
-            audioSource.Play();
             _clickCount = 0;
         }
     }
